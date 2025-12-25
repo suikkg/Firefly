e91c2e39901d2e9c9d91293aba90c9ebdd7ed5ca
suikkg <51974756+suikkg@users.noreply.github.com>
2025-12-25 14:15:15 +0000

    Deploying to pages from @ suikkg/Firefly@5bed98dc017245a14e47ca7897718b0b3e64e856 🚀

--- _astro/KatexManager.astro_astro_type_script_index_0_lang.COX3k4h_.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/katex.DvbnFR5o.css"])))=>i.map(i=>d[i]);
-import{_ as t}from"./preload-helper.BlOh4Y_i.js";function e(){document.querySelector(".katex")&&t(()=>Promise.resolve({}),__vite__mapDeps([0]))}e();document.addEventListener("swup:content:replace",e);
